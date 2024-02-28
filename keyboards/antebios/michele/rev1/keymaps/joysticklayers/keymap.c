6f953aced8653396f5c63ec42e338079a09b2592
Richard Nunez <Antebios1@gmail.com>
2024-02-28 15:01:46 -0600

    Change how I call Delete and Backspace

--- keyboards/antebios/michele/rev1/keymaps/joysticklayers/keymap.c
+++ keyboards/antebios/michele/rev1/keymaps/joysticklayers/keymap.c
@@ -427,7 +427,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_QWERTY] = LAYOUT(
-    TD(TD_DEL_BSPC),  KC_1,               KC_2,        KC_3,        KC_4,        KC_5,                              KC_6,       KC_7,        KC_8,     TD(TD_9_LPRN),   TD(TD_0_RPRN),   TD(TD_MINS_UNDS),
+    KC_BSPC,          KC_1,               KC_2,        KC_3,        KC_4,        KC_5,                              KC_6,       KC_7,        KC_8,     TD(TD_9_LPRN),   TD(TD_0_RPRN),   TD(TD_MINS_UNDS),
     TD(TD_TAB_TILDE), KC_Q,               KC_W,        KC_E,        KC_R,        KC_T,                              KC_Y,       KC_U,        KC_I,               KC_O,     KC_P,         TD(TD_EQL_PLUS),
     TD(TD_SHIFT_CAPS),LGUI_T(KC_A),LALT_T(KC_S),LSFT_T(KC_D),LCTL_T(KC_F),       KC_G,                              KC_H,RCTL_T(KC_J),RSFT_T(KC_K),       LALT_T(KC_L), TD(TD_SMCL_CLN), TD(TD_QUOT_DQT),
     TD(CTL_OSL1),     KC_Z,               KC_X,        KC_C,        KC_V,        KC_B,                              KC_N,       KC_M,TD(TD_COMM_LABK), TD(TD_DOT_RABK), TD(TD_SLSH_QUES),   KC_RSFT,
@@ -437,7 +437,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * ALT Layout
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | Ins  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F0  | F11  |
+ * | Ins  | Del  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F0  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | PrtSc|      |      |  UP  |      |  (   |                    |   )  |   7  |   8  |   9  |   /  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -451,7 +451,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_ALT] = LAYOUT(
-    KC_INS,  KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,   KC_F11,
+    KC_INS,  KC_DEL,  KC_F2,  KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,   KC_F11,
     KC_PSCR, KC_NO,   KC_NO,  KC_UP,   KC_NO,   KC_LPRN,                       KC_RPRN, KC_P7,  KC_P8,   KC_P9,   KC_PSLS,  KC_F12,
     KC_NO,   KC_NO,   KC_LEFT,KC_DOWN, KC_RIGHT,KC_LBRC,                       KC_RBRC, KC_P4,  KC_P5,   KC_P6,   KC_PAST, KC_PMNS,
     KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_LCBR,                       KC_RCBR, KC_P1,  KC_P2,   KC_P3,   KC_PEQL, KC_PPLS,
